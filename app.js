91a412b2f634d633bc664b35ceda9ec86202f4d5
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-01-29 14:33:35 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,17 +4,29 @@ const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
 
-const logDBConnection = () => {
-    console.log('DB connected');
-};
+// const logDBConnection = () => {
+//     console.log('DB connected');
+// };
 
-myEmitter.addListener('connected', logDBConnection);
+// myEmitter.addListener('connected', logDBConnection);
 
-setInterval(() => {
-    myEmitter.emit('connected');
-    myEmitter.removeListener('connected', logDBConnection);
-    myEmitter.emit('connected');
-}, 2000)
+// setInterval(() => {
+//     myEmitter.emit('connected');
+//     myEmitter.removeListener('connected', logDBConnection);
+//     myEmitter.emit('connected');
+// }, 2000)
 
 
+myEmitter.on('msg', (data) => {
+    console.log(`${data} Received`);
+})
 
+myEmitter.emit('msg', 'Some data');
+
+
+// once
+myEmitter.once('off', () => {
+    console.log('Once');
+});
+myEmitter.emit('off');
+myEmitter.emit('off');
